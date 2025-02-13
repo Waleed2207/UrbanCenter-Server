66fc6eff3c5943c81f316b431ec1ef9fdbde189e
waleedjohjah <waleedjohjah@gmail.com>
2025-02-13 19:57:27 +0200

    Final project v.0.0.6

--- routers/commentRoutes.js
+++ routers/commentRoutes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const { CommentController } = require('../controllers/commentController');
 const commentRoutes = express.Router();
 
-commentRoutes.post('/add', CommentController.addComment);
+//GET 
 commentRoutes.get('/report/category_comment/:user_id/:category', CommentController.getCommentsByUserAndCategory);
+
+//POST
+commentRoutes.post('/add', CommentController.addComment);
+
+//DELETE
 commentRoutes.delete('/report/category_comment/:report_id', CommentController.deleteCommentByReportId); // ✅ DELETE route
 
 
